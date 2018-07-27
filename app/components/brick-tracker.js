73449a9da4614f26bb896b42cfbf5d95c93052d4
A N, Sudhindra (sa021e) <sa021e@intl.att.com>
2018-07-27 19:54:37 +0530

    Updated as per the instructions

---
+++ app/components/brick-tracker.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	// brickWarehouse: Ember.inject.service('brick-warehouse')
+});
--- app/routes/store/lots-o-bricks.js
+++ app/routes/store/lots-o-bricks.js
@@ -1,28 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return {
-      blue: {
-        '2 x 2': {
-          quantity: 7,
-          price: 0.02
-        },
-        '2 x 4': {
-          quantity: 2,
-          price: 0.04
-        }
-      },
-      green: {
-        '2 x 4': {
-          quantity: 13,
-          price: 0.04
-        },
-        '2 x 8': {
-          quantity: 41,
-          price: 0.08
-        }
-      }
-    }
-  }
+  warehouse: Ember.inject.service('brick-warehouse'),
+  model(){
+		return this.get('warehouse').getOtherBricks();
+	}
 });
